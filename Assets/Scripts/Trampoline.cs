e9f8970b685727a1325abde1317739046ed26ba9
Gustavo Miller <42343653+MillerGS03@users.noreply.github.com>
2020-11-29 23:10:36 -0300

    Plataforma 1 criada

---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float force = 5;
+ 
+    private void OnTriggerEnter (Collider collider) {
+    if (collider.attachedRigidbody != null) {
+        collider.attachedRigidbody.velocity += transform.TransformDirection(new Vector3 (0, force, 0));
+    }
+}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
