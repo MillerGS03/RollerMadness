4f1a3ed556867de33cfacf11f323265a2cfddbe2
Scherer <fsvicentin@hotmail.com>
2020-12-02 00:06:46 -0300

    Octogono continado

--- Assets/Scripts/SetActiveParticlesWhenHit.cs
+++ Assets/Scripts/SetActiveParticlesWhenHit.cs
@@ -5,19 +5,45 @@ using UnityEngine;
 
 public class SetActiveParticlesWhenHit : MonoBehaviour
 {
+    public const int DELAY_SECONDS = 7;
     // Start is called before the first frame update
-    public GameObject target;
+    public GameObject trigger;
+    public GameObject[] targets;
     public GameObject collider;
 
-    public bool active;
+    bool lastActive = false;
+    DateTime count;
+
     void Start()
     {
     }
     // Update is called once per frame
     void Update()
     {
-        double distX = Math.Abs(target.transform.position.x - collider.transform.position.x);
-        double distY = Math.Abs(target.transform.position.y - collider.transform.position.y);
-        target.SetActive(Math.Sqrt(Math.Pow(distX,2) + Math.Pow(distY,2)) < 14f);
+        try
+        {
+            double distX = Math.Abs(trigger.transform.position.x - collider.transform.position.x);
+            double distY = Math.Abs(trigger.transform.position.z - collider.transform.position.z);
+            bool active = Math.Sqrt(Math.Pow(distX,2) + Math.Pow(distY,2)) < 14f;
+
+            bool changeToFalse = (active != lastActive) && !active;
+
+            if (changeToFalse)
+                count = System.DateTime.UtcNow;
+
+            if ((System.DateTime.UtcNow - count).Seconds > DELAY_SECONDS)
+            {
+                foreach(GameObject target in targets)
+                    target.SetActive(active);
+            }
+
+            lastActive = active;
+
+            Debug.Log(count+"");
+        }
+        catch (Exception ex)
+        {
+
+        }
     }
 }
