b62e129572a3bfb27ec243fb260b47ae57a55eeb
Gustavo Miller <42343653+MillerGS03@users.noreply.github.com>
2020-12-01 22:54:40 -0300

    Círculos dos Spawners

---
+++ Assets/Scripts/SetActiveParticlesWhenHit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveParticlesWhenHit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject target;
+    public GameObject collider;
+
+    public bool active;
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        double distX = Math.Abs(target.transform.position.x - collider.transform.position.x);
+        double distY = Math.Abs(target.transform.position.y - collider.transform.position.y);
+        target.SetActive(Math.Sqrt(Math.Pow(distX,2) + Math.Pow(distY,2)) < 14f);
+    }
+}
